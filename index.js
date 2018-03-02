943d4ce212047b76d9e169d43face182eec5348f
Greg Connell <greg.connell@ff0000.com>
2018-03-02 12:15:33 -0800

    moves webpack-rollup-babel-loader asset logic to wp-deploy-manager  - adds babel-eslint

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var log = debug('wp-plugin-assets')
 function AssetsPlugin(DM, options) {
 	this.DM = DM
 	this.options = options
-	this.prevFbaTimestamps = {}
 	/** TODO
 			Document `this.options`:
 
@@ -32,30 +31,21 @@ function AssetsPlugin(DM, options) {
 	*/
 }
 
-
 AssetsPlugin.prototype.apply = function(compiler) {
 	compiler.plugin('compile', () => {
 		// reset binary assets store on each compile
-		this.DM.payload.resetBinaryAssets()
+		this.DM.payload.store.reset()
 	})
 
 	compiler.plugin('emit', (compilation, callback) => {
-		var promises = [];
-
-		// can take in an array that'll be populated w/ binary assets found by Rollup
-
-		var addTimestamp = (asset) => {
-			var { mtimeMs: timestamp } = fs.statSync(asset.path)
-			return Object.assign({ timestamp }, asset)
-		}
-
-		var fbaAssets = this.DM.payload.getBinaryAssets().map(addTimestamp) || [];
+		var promises = []
+		var fbaAssets = []
 
 		// if any of the asset-payloads are dirty, the whole fba needs to be recompiled
 		var isDirty = false
 		for (var i in this.options.assets) {
 			const payload = this.options.assets[i].payload()
-			if (payload.dirty) {
+			if (payload && payload.type !== 'inline' && payload.dirty) {
 				isDirty = true
 				break
 			}
@@ -64,6 +54,8 @@ AssetsPlugin.prototype.apply = function(compiler) {
 		// iterate assets
 		for (var i in this.options.assets) {
 			const payload = this.options.assets[i].payload()
+			if (!payload) continue
+
 			payload.type = payload.type || 'copy'
 
 			// if payload type is an fba chunk-type
@@ -73,7 +65,7 @@ AssetsPlugin.prototype.apply = function(compiler) {
 					payload.modules.forEach(_module => {
 						fbaAssets.push({
 							chunkType: payload.type,
-							path: _module.userRequest
+							path: _module
 						})
 					})
 				}
@@ -82,7 +74,7 @@ AssetsPlugin.prototype.apply = function(compiler) {
 				if (payload.dirty) {
 					log('Inlining ->')
 					payload.modules.forEach(_module => {
-						log(` ${_module.userRequest}`)
+						log(` ${_module}`)
 					})
 				}
 			} else {
@@ -102,37 +94,11 @@ AssetsPlugin.prototype.apply = function(compiler) {
 			}
 		}
 
-		var isDirty = (fbaAsset) => {
-			var prevTimestamp = this.prevFbaTimestamps[fbaAsset.path]
-			var currTimestamp = fbaAsset.timestamp
-
-			if (!prevTimestamp) {
-				return true
-			}
-			else if (prevTimestamp !== currTimestamp) {
-				return true
-			}
-			return false
-		}
-
-		var anyDirty = (fbaAssets) => {
-			for (const asset of fbaAssets) {
-				if (isDirty(asset)) return true
-			}
-			return false
-		}
-
-		var anyDirtyFba = anyDirty(fbaAssets)
-
-		// update FBA timestamps for checking if FBA's were changed
-		this.updateFbaTimestamps(fbaAssets)
-
 		// compile all the assets
-		if (fbaAssets.length && anyDirtyFba) {
-			var payloadOutput = this.DM.payload.get().output
+		if (fbaAssets.length) {
 			promises.push(
 				fbaCompiler.compile({
-					target: path.resolve(`${payloadOutput.path}/${payloadOutput.filename}`),
+					target: path.resolve(`${this.options.output.path}/${this.options.output.filename}`),
 					assets: fbaAssets
 				})
 			)
@@ -151,11 +117,4 @@ AssetsPlugin.prototype.apply = function(compiler) {
 	})
 }
 
-AssetsPlugin.prototype.updateFbaTimestamps = function(fbaAssets) {
-	fbaAssets.forEach(asset => {
-		this.prevFbaTimestamps[asset.path] = asset.timestamp
-	})
-}
-
 module.exports = AssetsPlugin
-
