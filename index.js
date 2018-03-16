b90eeac08780671b6fe96f22bc9713040ca5af0c
Derek Miranda <derek.miranda@ff0000.com>
2018-03-15 17:09:58 -0700

    Consolidating binary import adding into this plugin now that binary assets found in module graph

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const path = require('path')
 const _ = require('lodash')
 
 const fbaCompiler = require('@ff0000-ad-tech/fba-compiler')
+const createBinaryImporter = require('binary-imports')
 const copier = require('./lib/copier.js')
 
 const debug = require('debug')
 var log = debug('wp-plugin-assets')
 
 function AssetsPlugin(DM, options) {
+	if (!options.addBinaryAsset || !options.fbaTypes) {
+		throw new Error('addBinaryAsset function and array of FBA types are needed to load binary assets into FBA payload')
+	} else if (!options.buildEntry) {
+		throw new Error("Assets Plugin requires the path to the 'build' entry")
+	}
+
 	this.DM = DM
 	this.options = options
-	this.prevFbaTimestamps = {}
+	this.loadBinaryImports = createBinaryImporter(options.fbaTypes, options.addBinaryAsset)
+
 	/** TODO
 			Document `this.options`:
 
@@ -32,107 +40,128 @@ function AssetsPlugin(DM, options) {
 	*/
 }
 
+function getDepsWithState(initState) {
+	const { resourceStack = [], modulesByResource, buildDeps = [], seenModules = {} } = initState
+
+	if (!resourceStack.length) {
+		return initState
+	}
+
+	const currResource = resourceStack[resourceStack.length - 1]
+	const currModule = modulesByResource[currResource]
+
+	if (currModule && !seenModules[currResource]) {
+		seenModules[currResource] = true
+		const depModules = currModule.dependencies.map(d => d.module).filter(a => a)
+		const depResources = depModules.map(dm => dm.resource)
+		const newBuildDeps = buildDeps.concat(depResources)
+		const newResourceStack = resourceStack
+			// remove currModule from stack
+			.slice(0, -1)
+			// put subdeps on stack
+			.concat(depResources)
+		const newState = getDepsWithState({
+			resourceStack: newResourceStack,
+			buildDeps: newBuildDeps,
+			modulesByResource,
+			seenModules
+		})
+		return newState
+	}
+
+	return initState
+}
+
+function getDepsRecursively(topResource, modulesByResource) {
+	const result = getDepsWithState({
+		resourceStack: [topResource],
+		modulesByResource
+	})
+	const { buildDeps } = result
+	return buildDeps.filter(a => a)
+}
 
 AssetsPlugin.prototype.apply = function(compiler) {
 	compiler.plugin('compile', () => {
 		// reset binary assets store on each compile
-		this.DM.payload.resetBinaryAssets()
+		this.DM.payload.store.reset()
+	})
+
+	compiler.plugin('after-compile', (compilation, callback) => {
+		const { buildEntry } = this.options
+		const buildModule = compilation.modules.find(m => m.resource && m.resource === buildEntry)
+
+		/*
+			Gathering all of the filepaths within the build's dependency graph to pass into
+			a function which filters out non-binary assets and formats binary assets
+			for the FBA compiler
+
+			When using the Rollup Babel loader (i.e. production settings),
+			this Array should contain all of the build's dependencies in a flat array
+		*/
+		const fileDeps = buildModule.fileDependencies
+
+		/* 
+			However, when using just the Babel loader (i.e. debug settings),
+			the other dependencies are not included in the prior array
+
+			So we'll need to search for these recursively
+		*/
+		const modulesByResource = compilation.modules.reduce((accum, module) => {
+			if (module.resource) {
+				accum[module.resource] = module
+			}
+			return accum
+		}, {})
+		const recursivelyFoundResources = getDepsRecursively(buildEntry, modulesByResource)
+
+		const allFileDeps = fileDeps.concat(recursivelyFoundResources)
+
+		allFileDeps.forEach(this.loadBinaryImports)
+
+		callback()
 	})
 
 	compiler.plugin('emit', (compilation, callback) => {
-		var promises = [];
+		var promises = []
+		var fbaAssets = []
 
-		// can take in an array that'll be populated w/ binary assets found by Rollup
-
-		var addTimestamp = (asset) => {
-			var { mtimeMs: timestamp } = fs.statSync(asset.path)
-			return Object.assign({ timestamp }, asset)
-		}
-
-		var fbaAssets = this.DM.payload.getBinaryAssets().map(addTimestamp) || [];
-
-		// if any of the asset-payloads are dirty, the whole fba needs to be recompiled
-		var isDirty = false
-		for (var i in this.options.assets) {
-			const payload = this.options.assets[i].payload()
-			if (payload.dirty) {
-				isDirty = true
-				break
-			}
-		}
+		const anyDirty = this.DM.payload.store.anyDirty()
 
 		// iterate assets
 		for (var i in this.options.assets) {
 			const payload = this.options.assets[i].payload()
+			if (!payload) continue
+
 			payload.type = payload.type || 'copy'
 
-			// if payload type is an fba chunk-type
 			if (payload.type.match(/^fbA/i)) {
-				if (isDirty || payload.dirty) {
-					// append content to fba-compiler
-					payload.modules.forEach(_module => {
+				// if payload type is an fba chunk-type
+				if (anyDirty) {
+					Object.keys(payload.modules).forEach(path => {
 						fbaAssets.push({
 							chunkType: payload.type,
-							path: _module.userRequest
+							path: path
 						})
 					})
 				}
 			} else if (payload.type == 'inline') {
 				// if payload type is inline
-				if (payload.dirty) {
-					log('Inlining ->')
-					payload.modules.forEach(_module => {
-						log(` ${_module.userRequest}`)
-					})
-				}
-			} else {
-				// if payload type is copy
-				if (payload.dirty) {
-					// copy the asset to deploy
-					promises.push(copier.copy(payload.modules, this.options.assets[i].copy))
-				}
-			}
-
-			// mark this payload clean
-			if (payload.name) {
-				this.DM.payload.store.update({
-					name: payload.name,
-					dirty: false
+				log('Inlining ->')
+				Object.keys(payload.modules).forEach(path => {
+					log(` ${path}`)
 				})
+			} else {
+				// copy the asset to deploy
+				promises.push(copier.copy(payload.modules, this.options.assets[i].copy))
 			}
 		}
-
-		var isDirty = (fbaAsset) => {
-			var prevTimestamp = this.prevFbaTimestamps[fbaAsset.path]
-			var currTimestamp = fbaAsset.timestamp
-
-			if (!prevTimestamp) {
-				return true
-			}
-			else if (prevTimestamp !== currTimestamp) {
-				return true
-			}
-			return false
-		}
-
-		var anyDirty = (fbaAssets) => {
-			for (const asset of fbaAssets) {
-				if (isDirty(asset)) return true
-			}
-			return false
-		}
-
-		var anyDirtyFba = anyDirty(fbaAssets)
-
-		// update FBA timestamps for checking if FBA's were changed
-		this.updateFbaTimestamps(fbaAssets)
 
 		// compile all the assets
-		if (fbaAssets.length && anyDirtyFba) {
-			var payloadOutput = this.DM.payload.get().output
+		if (fbaAssets.length) {
 			promises.push(
 				fbaCompiler.compile({
-					target: path.resolve(`${payloadOutput.path}/${payloadOutput.filename}`),
+					target: path.resolve(`${this.options.output.path}/${this.options.output.filename}`),
 					assets: fbaAssets
 				})
 			)
@@ -151,11 +180,4 @@ AssetsPlugin.prototype.apply = function(compiler) {
 	})
 }
 
-AssetsPlugin.prototype.updateFbaTimestamps = function(fbaAssets) {
-	fbaAssets.forEach(asset => {
-		this.prevFbaTimestamps[asset.path] = asset.timestamp
-	})
-}
-
 module.exports = AssetsPlugin
-
