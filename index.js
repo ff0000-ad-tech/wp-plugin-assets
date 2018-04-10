b59cb59b4bf8b5dcc2f74ed5e6f4e49d8fdad8df
Greg Connell <greg.connell@gregc-mbp.ff0000.com>
2018-04-09 17:25:03 -0700

    repaths import for @ff0000-ad-tech/debug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fbaCompiler = require('@ff0000-ad-tech/fba-compiler')
 const createBinaryImporter = require('binary-imports')
 const copier = require('./lib/copier.js')
 
-const debug = require('debug')
+const debug = require('@ff0000-ad-tech/debug')
 var log = debug('wp-plugin-assets')
 
 function AssetsPlugin(DM, options) {
