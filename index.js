1c3e34a5aa265440b039741fb10db64139b96982
Derek Miranda <derek.miranda@ff0000.com>
2018-11-08 15:47:10 -0800

    Following issuer chain of modules to see if it's within the same dependency tree as the build entry
    - fixed bug where no images showed up in payload when using Webpack dependencies after Ad.js paradigm shift
    - this check for image/font dependencies adds to the current checks

--- index.js
+++ index.js
@@ -14,137 +14,150 @@ var log = debug('wp-plugin-assets')
 const pluginName = 'FAT Assets Plugin'
 
 function AssetsPlugin(DM, options) {
-	if (!options.addBinaryAsset || !options.fbaTypes) {
-		throw new Error('addBinaryAsset function and array of FBA types are needed to load binary assets into FBA payload')
-	} else if (!options.buildEntry) {
-		throw new Error("Assets Plugin requires the path to the 'build' entry")
-	}
+  if (!options.addBinaryAsset || !options.fbaTypes) {
+    throw new Error('addBinaryAsset function and array of FBA types are needed to load binary assets into FBA payload')
+  } else if (!options.buildEntry) {
+    throw new Error("Assets Plugin requires the path to the 'build' entry")
+  }
 
-	this.DM = DM
-	this.options = options
-	this.loadBinaryImports = createBinaryImporter(options.fbaTypes, options.addBinaryAsset)
+  this.DM = DM
+  this.options = options
+  this.loadBinaryImports = createBinaryImporter(options.fbaTypes, options.addBinaryAsset)
 
-	/** TODO
-			Document `this.options`:
+  /** TODO
+      Document `this.options`:
 
-			this.options.assets = [
-				{
-					payload: [
-						// payload objects (see wp-plugin-payload)
-						{
-							modules: [] // subpaths to file locations
-						}
-					],
-					copy: {
-						from: './source-context',
-						to: './destination-context'
-					}
-				}
-			]
-	*/
-}
+      this.options.assets = [
+        {
+          payload: [
+            // payload objects (see wp-plugin-payload)
+            {
+              modules: [] // subpaths to file locations
+            }
+          ],
+          copy: {
+            from: './source-context',
+            to: './destination-context'
+          }
+        }
+      ]
+      */
+          }
 
 AssetsPlugin.prototype.apply = function(compiler) {
-	compiler.hooks.compile.tap(pluginName, () => {
-		// reset binary assets store on each compile
-		this.DM.payload.store.reset()
-	})
+  compiler.hooks.compile.tap(pluginName, () => {
+    // reset binary assets store on each compile
+    this.DM.payload.store.reset()
+  })
 
-	compiler.hooks.afterCompile.tapAsync(pluginName, (compilation, callback) => {
-		const { buildEntry } = this.options
+  compiler.hooks.afterCompile.tapAsync(pluginName, (compilation, callback) => {
+    const { buildEntry } = this.options
 
-		const buildModule = compilation.entries.find(m => m.resource && m.resource === buildEntry)
-		/* 
-			When using just the Babel loader (i.e. debug settings),
-			the other dependencies are not included in the prior array
+    const buildModule = compilation.entries.find(m => m.resource && m.resource === buildEntry)
+    /* 
+      When using just the Babel loader (i.e. debug settings),
+      the other dependencies are not included in the prior array
 
-			So we'll need to search for these recursively
-		*/
-		const modulesByResource = compilation.modules.reduce((accum, module) => {
-			if (module.resource) {
-				accum[module.resource] = module
-			}
-			return accum
-		}, {})
-		const resourcesOnBuildEntry = getDepsFromModule(buildEntry, modulesByResource)
+      So we'll need to search for these recursively
+      */
+    const modulesByResource = compilation.modules.reduce((accum, module) => {
+      if (module.resource) {
+        accum[module.resource] = module
+      }
+      return accum
+    }, {})
+    const resourcesOnBuildEntry = getDepsFromModule(buildEntry, modulesByResource)
 
-		/* 
-			Build modules have to be found differently with the Rollup Babel loader.
-			They can't be found as dependencies of the build.js entry
-			so we'll have to check for compilation modules that list the build entry module
-			as a ModuleReason
-		*/
-		const resourcesWithBuildEntryReason = getDepsWithModuleReason(compilation.modules, buildModule)
+    /* 
+      Build modules have to be found differently with the Rollup Babel loader.
+      They can't be found as dependencies of the build.js entry
+      so we'll have to check for compilation modules that list the build entry module
+      as a ModuleReason
+      */
+    const resourcesWithBuildEntryReason = getDepsWithModuleReason(compilation.modules, buildModule)
 
-		const allDeps = resourcesOnBuildEntry.concat(resourcesWithBuildEntryReason)
-		allDeps.forEach(this.loadBinaryImports)
+    /*
+      Get modules which have the build entry as the issuer
+      */
+    const modulesWithIssuerBuildEntry = compilation.modules
+      .filter(function checkIssuer(m) {
+        if (!m.issuer) return false
+        // log('m.issuer.resource', m.issuer.resource)
+        return m.issuer.resource === buildEntry || checkIssuer(m.issuer)
+      })
+      .map(m => m.resource)
 
-		// if there were binary assets, update ad.settings with the payload filename
-		if (this.DM.payload.store.anyFba()) {
-			this.options.output.hasFbaAssets(this.options.output.filename)
-		} else {
-			this.options.output.hasFbaAssets(false)
-		}
+    const allDeps = resourcesOnBuildEntry
+      .concat(resourcesWithBuildEntryReason)
+      .concat(modulesWithIssuerBuildEntry)
+    allDeps.forEach(this.loadBinaryImports)
 
-		callback()
-	})
+    // if there were binary assets, update ad.settings with the payload filename
+    if (this.DM.payload.store.anyFba()) {
+      this.options.output.hasFbaAssets(this.options.output.filename)
+    } else {
+      this.options.output.hasFbaAssets(false)
+    }
 
-	compiler.hooks.emit.tapAsync(pluginName, (compilation, callback) => {
-		var promises = []
-		var fbaAssets = []
+    callback()
+  })
 
-		const anyDirty = this.DM.payload.store.anyDirty()
+  compiler.hooks.emit.tapAsync(pluginName, (compilation, callback) => {
+    var promises = []
+    var fbaAssets = []
 
-		// iterate assets
-		for (var i in this.options.assets) {
-			const payload = this.options.assets[i].payload()
-			if (!payload) continue
+    const anyDirty = this.DM.payload.store.anyDirty()
 
-			payload.type = payload.type || 'copy'
+    // iterate assets
+    for (var i in this.options.assets) {
+      const payload = this.options.assets[i].payload()
+      if (!payload) continue
 
-			if (payload.type.match(/^fbA/i)) {
-				// if payload type is an fba chunk-type
-				if (anyDirty) {
-					Object.keys(payload.modules).forEach(path => {
-						fbaAssets.push({
-							chunkType: payload.type,
-							path: path
-						})
-					})
-				}
-			} else if (payload.type == 'inline') {
-				// if payload type is inline
-				log('Inlining ->')
-				Object.keys(payload.modules).forEach(path => {
-					log(` ${path}`)
-				})
-			} else {
-				// copy the asset to deploy
-				promises.push(copier.copy(payload.modules, this.options.assets[i].copy))
-			}
-		}
+      payload.type = payload.type || 'copy'
 
-		// compile all the assets
-		if (fbaAssets.length) {
-			promises.push(
-				fbaCompiler.compile({
-					target: path.resolve(`${this.options.output.path}/${this.options.output.filename}`),
-					assets: fbaAssets
-				})
-			)
-		}
+      if (payload.type.match(/^fbA/i)) {
+        // if payload type is an fba chunk-type
+        if (anyDirty) {
+          Object.keys(payload.modules).forEach(path => {
+            fbaAssets.push({
+              chunkType: payload.type,
+              path: path
+            })
+          })
+        }
+      } else if (payload.type == 'inline') {
+        // if payload type is inline
+        log('Inlining ->')
+        Object.keys(payload.modules).forEach(path => {
+          log(` ${path}`)
+        })
+      } else {
+        // copy the asset to deploy
+        promises.push(copier.copy(payload.modules, this.options.assets[i].copy))
+      }
+    }
 
-		// TODO: <img> and background-image declarations would have to be rewritten to payload blobs
+    // compile all the assets
+    if (fbaAssets.length) {
+      promises.push(
+        fbaCompiler.compile({
+          target: path.resolve(`${this.options.output.path}/${this.options.output.filename}`),
+          assets: fbaAssets
+        })
+      )
+    }
 
-		// return to webpack flow
-		Promise.all(promises)
-			.then(() => {
-				callback()
-			})
-			.catch(err => {
-				log(err)
-			})
-	})
+    // TODO: <img> and background-image declarations would have to be rewritten to payload blobs
+
+    // return to webpack flow
+    Promise.all(promises)
+      .then(() => {
+        callback()
+      })
+      .catch(err => {
+        log(err)
+      })
+  })
 }
 
 module.exports = AssetsPlugin
